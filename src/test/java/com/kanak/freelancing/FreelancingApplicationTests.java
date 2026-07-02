8cba0efcb13a52b2f55aa345a0484eee9bba61ff
Kanak <{ksao180205@gmail.com}>
2026-07-02 13:17:43 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/kanak/freelancing/FreelancingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanak.freelancing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
